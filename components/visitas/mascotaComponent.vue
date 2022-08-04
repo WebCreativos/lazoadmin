33b6fa8209edd1f53dd2449d4b19e6105890751a
brupadiro <brunodiharce@gmail.com>
2022-08-04 17:00:25 -0300

    f

--- components/visitas/mascotaComponent.vue
+++ components/visitas/mascotaComponent.vue
@@ -63,7 +63,7 @@
         </v-card>
       </v-card-text>
       <v-card-actions class="d-flex justify-center">
-        <v-pagination :total-visible="10" v-model="page"></v-pagination>
+        <v-pagination :total-visible="10" :length="Math.ceil(value.length/10)" v-model="page"></v-pagination>
       </v-card-actions>
     </v-card>
     <v-dialog v-model="modalData.openModal" width="80%" height="auto">
@@ -322,7 +322,9 @@
         if (atencion) {
           return atencion
         }
-        return {}
+        return {
+          length:0
+        }
       },
       consultaItems() {
         return this.$store.getters['atentions/getList']
